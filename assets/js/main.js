d6bef42ec98d5e454c13e457cfa9bf98465b4f15
Divyansh Tripathi <divyanshtripathi1998@gmail.com>
2022-12-22 18:38:58 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,7 +242,7 @@
    */
   window.addEventListener('load', () => {
     AOS.init({
-      duration: 1000,
+      duration: 200,
       easing: 'ease-in-out',
       once: true,
       mirror: false
